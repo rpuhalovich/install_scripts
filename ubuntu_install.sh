5b9cb5a9d7fe86dc65816bc808ed89f81a14f9d6
Ryan Puhalovich <reanpuhalovich@gmail.com>
2021-04-13 10:29:56 +1000

    added installs.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,17 +1,45 @@
 
 sudo apt update -y && sudo apt upgrade -y
 
-# --- install cmake ---
+sudo apt install -y gnome-sushi
+sudo snap install discord mailspring vlc spotify bitwarden signal gdebi-core 
+sudo snap install intellij-idea-community --classic --edge
+
+# brave
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install -y brave-browser
+
+# github desktop 
+sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.6.3-linux1/GitHubDesktop-linux-2.6.3-linux1.deb
+sudo gdebi GitHubDesktop-linux-2.6.3-linux1.deb
+
+# signal 
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+sudo mv signal-desktop-keyring.gpg /usr/share/keyrings/
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
+
+# cmake 
 CMAKE_VER=3.20.1
 wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}-linux-x86_64.sh
 sudo sh cmake-${CMAKE_VER}-linux-x86_64.sh --prefix=/usr/local --exclude-subdir
 rm cmake-${CMAKE_VER}-linux-x86_64.sh
 
-# ---- install albert ---
+# albert 
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 sudo apt update
 sudo apt install albert
 
-sudo snap install discord mailspring vlc spotify
+# joplin
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+
+# pandoc
+PANDOC_VER=2.13
+wget https://github.com/jgm/pandoc/releases/download/${PANDOC_VER}/pandoc-${PANDOC_VER}-1-amd64.deb
+dpkg -i pandoc-${PANDOC_VER}-1-amd64.deb
+rm pandoc-${PANDOC_VER}-1-amd64.deb
