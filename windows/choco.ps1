573f7dd673499ead1c3d65577c4090dd2a8408bc
Ryan P <reanpuhalovich@gmail.com>
2021-06-01 13:58:22 +1000

    Cleaned up ps1 script.

--- windows/choco.ps1
+++ windows/choco.ps1
@@ -1,8 +1,5 @@
-# This must be run from within a Powershell window opened with admin privileges.
-
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y geforce-experience brave 7zip vlc git foxitreader vscode steam discord google-backup-and-sync 
 choco install -y spotify windirstat transmission zoom pandoc typora autohotkey zotero hyper bitwarden micro astyle 
 choco install -y signal blender mailspring
-
