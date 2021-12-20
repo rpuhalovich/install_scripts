b7bd3eeb8c70f41db4f57117d9a0bbdfc8162790
rpuhalovich <reanpuhalovich@gmail.com>
2021-12-21 10:36:58 +1100

    update mac_install.sh

--- macos/mac_install.sh
+++ macos/mac_install.sh
@@ -5,7 +5,5 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install apps
-brew install pandoc &&\
-brew cask install dashlane visual-studio-code brave-browser discord typora \
-krita gimp google-photos-backup-and-sync spotify transmission zoomus zotero \
-hiddenbar rectangle blender signal cloc
+brew install pandoc visual-studio-code firefox discord typora gimp spotify transmission zoomus \
+hiddenbar rectangle signal cloc keka
